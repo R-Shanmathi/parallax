ab4f116206aa746f9e3768b323f6938aa48f96cf
R-Shanmathi <136781569+R-Shanmathi@users.noreply.github.com>
2023-10-08 06:36:37 +0530

    Add files via upload

---
+++ html24/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("scroll", function () {
+    const scrollPosition = window.scrollY;
+
+    document.querySelectorAll(".parallax-background").forEach(function (background) {
+        const speed = background.getAttribute("data-speed");
+        background.style.transform = `translateY(${scrollPosition * speed}px)`;
+    });
+});
+
